3424b54b156ad64b5838ddbcbf42fa5f7b003762
Gabriel Queiroz <gabriel-cso@hotmail.com>
2022-10-10 21:19:33 -0300

    create project twitter-client

---
+++ src/test/java/br/com/queiroz/twitter/TwitterClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.queiroz.twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
